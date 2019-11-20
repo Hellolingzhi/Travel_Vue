909eab3ebf5bb68bfdfa6162597bb19f453fde75
lingzhi he <1015244906@qq.com>
2019-11-20 18:33:49 +0800

    add search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varible.styl'
+.search
+  height: .72rem
+  padding: 0 .1rem
+  background-color: $bgColor
+  .search-input
+    box-sizing: border-box
+    width: 100%
+    padding: 0 .1rem
+    height: .6rem
+    line-height: .6rem
+    text-alie: center
+    color: #666
+    border-radius: .06rem
+</style>
